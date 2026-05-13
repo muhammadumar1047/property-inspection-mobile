99ab87d312b4bb440d14b81636876d19860b6269
Synergy Coders <synergycoders@Synergys-Mac-mini.local>
2026-05-13 12:59:34 +0500

    Initial commit

---
+++ ios/Runner/AppDelegate.swift
@@ -0,0 +1,17 @@
+import Flutter
+import UIKit
+import GoogleMaps
+@main
+@objc class AppDelegate: FlutterAppDelegate, FlutterImplicitEngineDelegate {
+  override func application(
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+  ) -> Bool {
+    GMSServices.provideAPIKey("AIzaSyBPSWQCU0Rz-WcUz-VjqquU2G8330Dcf0Q")
+    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+
+  func didInitializeImplicitFlutterEngine(_ engineBridge: FlutterImplicitEngineBridge) {
+    GeneratedPluginRegistrant.register(with: engineBridge.pluginRegistry)
+  }
+}
